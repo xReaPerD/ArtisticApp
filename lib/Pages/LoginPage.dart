02d1907ff951b389fc9d4be11d9c285273754770
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-10-22 11:05:15 +0530

    Updated

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:main_working_project/Pages/AddProfileImagePage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,8 +14,145 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+          backgroundColor: const Color(0xFFE8A9A7), //peachy pink
           body: Center(
-            child: Text("Login"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(padding: EdgeInsets.only(left: 20),
+                          child: Text("Create\nAn Account",style:
+                          TextStyle(
+                              fontSize: 45,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w600
+                          ),),
+                        )
+                    ),
+
+                    SizedBox(height: 30,),
+
+                    Container(
+                      width: 280,
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade100.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.blueAccent,width: 1.5)
+                      ),
+                      child: Padding(padding: EdgeInsets.only(left: 10,right: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Username",
+                              border: InputBorder.none,
+                              icon: Icon(Icons.person)
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20,),
+
+                    Container(
+                      width: 280,
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade100.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.blueAccent,width: 1.5)
+                      ),
+                      child: Padding(padding: EdgeInsets.only(left: 10,right: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Email",
+                              border: InputBorder.none,
+                              icon: Icon(Icons.email)
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20,),
+
+                    Container(
+                      width: 280,
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade100.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.blueAccent,width: 1.5)
+                      ),
+                      child: Padding(padding: EdgeInsets.only(left: 10,right: 10),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              border: InputBorder.none,
+                              icon: Icon(Icons.lock)
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 20,),
+
+                    Container(
+                      width: 280,
+                      decoration: BoxDecoration(
+                          color: Colors.pink.shade100.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.blueAccent,width: 1.5)
+                      ),
+                      child: Padding(padding: EdgeInsets.only(left: 10,right: 10),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: "Confirm Password",
+                              border: InputBorder.none,
+                              icon: Icon(Icons.lock)
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const AddProfileImagePage()));
+                  },
+                  child: Container(
+                    width: 260,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent.shade100,
+                      border: Border.all(color: Colors.black87,width: 1),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: CupertinoColors.darkBackgroundGray.withOpacity(0.3),
+                          blurRadius: 3,
+                          spreadRadius: 1,
+                          offset: const Offset(0, 3),
+                        )
+                      ],
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    ),
+                  ),
+              ],
+            )
           ),
         ),
     );
